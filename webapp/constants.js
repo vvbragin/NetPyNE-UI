39b92e58fdce105b7b9438589db270a42fb7efc1
enicolasgomez <enicolasgomez@gmail.com>
2022-03-09 10:01:28 -0300

    changed granger plot method name

--- webapp/constants.js
+++ webapp/constants.js
@@ -343,7 +343,7 @@ export const PLOT_WIDGETS = {
     disabled: true,
     method: {
       plotKey: 'granger',
-      plotMethod: 'iplotGranger',
+      plotMethod: 'granger',
       plotType: false,
     },
     pos: 11,
@@ -352,7 +352,7 @@ export const PLOT_WIDGETS = {
       name: 'Granger Plot',
       method: {
         plotKey: 'granger',
-        plotMethod: 'iplotGranger',
+        plotMethod: 'granger',
         plotType: false,
       },
     },
